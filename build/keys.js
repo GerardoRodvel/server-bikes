e0ef08192fdc0b26e495fc73080459a09ea34e69
GerardoRodvel <jose_gerardo_lpz@hotmail.com>
2019-12-08 19:37:57 -0600

    database

--- build/keys.js
+++ build/keys.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'ng_bikes_db'
+        host: 'bv7bfkxavuh9rist4zdk-mysql.services.clever-cloud.com',
+        user: 'uaqsqozoub78jm9b',
+        password: 'cQT4Uw9mHTs3MmnswmcH',
+        database: 'bv7bfkxavuh9rist4zdk'
     }
 };
--- src/keys.ts
+++ src/keys.ts
@@ -1,9 +1,8 @@
 export default {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'ng_bikes_db'
-
+        host: 'bv7bfkxavuh9rist4zdk-mysql.services.clever-cloud.com',
+        user: 'uaqsqozoub78jm9b',
+        password: 'cQT4Uw9mHTs3MmnswmcH',
+        database: 'bv7bfkxavuh9rist4zdk'
     }
 }
